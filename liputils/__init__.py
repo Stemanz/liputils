4d66ae22fe373f64dbca8784255f27eefe961470
Stefano Manzini <stefano.manzini@gmail.com>
2021-03-16 18:04:12 +0100

    Update __init__.py

--- liputils/__init__.py
+++ liputils/__init__.py
@@ -2,4 +2,4 @@ from .liputils import Lipid, make_residues_table, saturated, max_carbon, GUI
 
 __all__ = ("Lipid", "make_residues_table", "saturated", "max_carbon", "GUI")
 
-__version__ = "0.16.1"
+__version__ = "0.16.2"
